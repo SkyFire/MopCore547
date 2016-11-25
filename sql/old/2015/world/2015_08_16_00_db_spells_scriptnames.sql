99087449b2394c94c327d7e5e0fcf28e5c30dc9e
cooler-SAI <cooler19860212@yahoo.com>
2016-11-25 22:12:50 +0300

    First Commit

---
+++ sql/old/2015/world/2015_08_16_00_db_spells_scriptnames.sql
@@ -0,0 +1,5 @@
+REPLACE INTO spell_script_names VALUES ('585', 'spell_pri_from_darkness_comes_light');
+REPLACE INTO spell_script_names VALUES ('2050', 'spell_pri_from_darkness_comes_light');
+REPLACE INTO spell_script_names VALUES ('2061', 'spell_pri_from_darkness_comes_light');
+REPLACE INTO spell_script_names VALUES ('32546', 'spell_pri_from_darkness_comes_light');
+REPLACE INTO spell_script_names VALUES ('2060', 'spell_pri_from_darkness_comes_light');
